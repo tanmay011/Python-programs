a483aafb89459763caddd34f5d365887c2470881
Tanmay Naik <55075402+tanmay011@users.noreply.github.com>
2020-09-10 20:34:52 +0530

    Add files via upload

---
+++ Execution_time_of_program.py
@@ -0,0 +1,24 @@
+# import time
+
+# def fun1(a,b):
+    
+#     return a+b
+
+
+# def fun2(a,b):
+    
+#      a
+#      b
+#      sum(a,b)
+#      return a+b
+
+
+# if __name__ == "__main__":
+#     init = time()
+#     fun1(3,5):
+
+#           print("rohan das time", time()-init)
+#     init = time()
+#     fun2(3,5):
+
+#          print("tanmay time", time()-init)
---
+++ palindrome_program.py
@@ -0,0 +1,14 @@
+print("enter your no")
+number = int((input()))
+temp = number
+reverse = 0
+
+while(number>0):
+    dig = number%10
+    reverse = reverse*10 + dig
+    number = number//10
+
+if temp == reverse:
+    print("It is palindrome")
+else:
+    print("not palindrome")
